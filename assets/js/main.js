5d8359b4bf50a4205b14f4da455aa6ab717ec89f
i6rahim <i6rahim210@gmail.com>
2023-01-28 22:25:06 +0300

    "add"

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,18 @@ const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-    slidesPerView: 4,
+    slidesPerView: 5,
+    // spaceBetween: 25,
+    // centeredSlides: true,
+    initialSlide: 0,
     navigation: {
       nextEl: '.swiper-button-next'
     },
     
-  });
+});
+
+
+const allSlides = document.querySelectorAll('.swiper-slide');
+allSlides.forEach(el => {
+    
+})
