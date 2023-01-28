3c7306aaaf9947f6698ce5aa8e8094b1173c86c0
i6rahim <i6rahim210@gmail.com>
2023-01-28 16:58:13 +0300

    "add"

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 4,
+    navigation: {
+      nextEl: '.swiper-button-next'
+    },
+    
+  });
