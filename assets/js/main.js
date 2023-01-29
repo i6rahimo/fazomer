9c979f4a654d64b29958791201c442bc97fd75ae
i6rahim <i6rahim210@gmail.com>
2023-01-29 14:51:33 +0300

    "add"

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,16 +9,48 @@ const swiper = new Swiper('.swiper', {
     navigation: {
       nextEl: '.swiper-button-next'
     },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 2,
+      },
+      900: {
+        slidesPerView: 3,
+ 
+      },
+      // when window width is >= 480px
+      1200: {
+        slidesPerView: 3,
+      },
+      // when window width is >= 640px
+      1500: {
+        slidesPerView: 5,
+      }
+    }
     
 });
 
-
-const allSlides = document.querySelectorAll('.swiper-slide');
-allSlides.forEach(el => {
-    
-})
 
 const tel = document.getElementById("phone");
 
 var im = new Inputmask("+7-(999)-999-99-99");
 im.mask(tel);
+
+
+
+const commentSwiper = new Swiper('.comment__swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  wrapperClass: 'comment__swiper-wrapper',
+  slideClass: 'comment__swiper-slide',
+  slidesPerView: 2,
+  spaceBetween: 75,
+  navigation: {
+    nextEl: '.comment__swiper-button-next',
+    prevEl: '.comment__swiper-button-prev'
+  },
+  
+});
+
+
