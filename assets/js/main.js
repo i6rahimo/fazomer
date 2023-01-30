4c4e9e843b30a64381c2deaa400694e72d79df4b
i6rahim <i6rahim210@gmail.com>
2023-01-30 07:29:55 +0300

    "add"

--- assets/js/main.js
+++ assets/js/main.js
@@ -70,6 +70,16 @@ const commentSwiper = new Swiper('.comment__swiper', {
     },
   }
 });
-
-
 });
+
+
+function burgerMenu() {
+  const burgerBtn = document.querySelector('.burger');
+  const menu = document.querySelector('.header__nav');
+  
+  burgerBtn.addEventListener('click', ()=> {
+    menu.classList.toggle('active');
+    burgerBtn.classList.toggle('active')
+  })
+}
+burgerMenu()
